d21011a8a5e583ca44589be2888d10f26a88a125
Geraint Jones <geraintrjones@gmail.com>
2018-03-26 14:48:57 +0100

    Add keyboard WASD component and system

--- src/main.js
+++ src/main.js
@@ -1,10 +1,14 @@
 import kd from 'keydrown';
 import ecs from 'tiny-ecs';
 import rafloop from 'raf-loop'
-// systems
-import Position from './Position'
 
 // components
+import Position from './Position'
+import WASD from './WASD'
+
+
+// systems
+import wasdSystem from './wasdSystem'
 
 const glslify = require('glslify');
 // needed for bug https://github.com/stackgl/glslify/issues/49 - if you try using fixes like glslify babel plugin, then shaders wont live reload!!
@@ -16,6 +20,7 @@ const ents = new ecs.EntityManager(); // ents, because, i keep misspelling entit
 const player = ents.createEntity();
 
 player.addComponent(Position);
+player.addComponent(WASD);
 
 player.position.y = 8;
 
@@ -24,8 +29,8 @@ player.position.y = 8;
 
 const engine = rafloop(function(dt) {
     kd.tick();
-    ents.queryComponents([Position]).forEach((each) => {
-      console.log(each);
+    ents.queryComponents([Position, WASD]).forEach((each) => {
+      wasdSystem(each,[kd.W.isDown(), kd.A.isDown(),kd.S.isDown(),kd.D.isDown()])
     });
 
 }).start()
